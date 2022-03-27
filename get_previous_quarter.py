77c19971a6de323262d8f8fbdc36c7a2acb235c6
heyitssree <sreeamaravila@live.com>
2022-03-27 20:21:51 +0530

    Updated input parameter type

--- get_previous_quarter.py
+++ get_previous_quarter.py
@@ -1,13 +1,12 @@
-"""Function to retreive the previous quarter, based on the provided date."""
 def get_prev_quarter(yyyy, mm, dd):
-    if((mm==1) or (mm==2) or (mm==3)):
-        quarter_date = f"""{yyyy-1}-12-31"""
-    elif((mm==4) or (mm==5) or (mm==6)):
+    if((mm=="01") or (mm=="02") or (mm=="03")):
+        quarter_date = f"""{int(yyyy)-1}-12-31"""
+    elif((mm=="04") or (mm=="05") or (mm=="06")):
         quarter_date = f"""{yyyy}-03-31"""
-    elif((mm==7) or (mm==8) or (mm==9)):
+    elif((mm=="07") or (mm=="08") or (mm=="09")):
         quarter_date = f"""{yyyy}-06-30"""
-    elif((mm==10) or (mm==11) or (mm==12)):
+    elif((mm=="10") or (mm=="11") or (mm=="12")):
         quarter_date = f"""{yyyy}-09-30"""
     return (quarter_date)
     
-print(get_prev_quarter(2025,1,27))
+print(get_prev_quarter("2025","02","27"))
